0e98d150c6bbc3e70a1fef28d64374f3729f2d71
Adam Aboelmatty <adamaboelmatty@Adams-MacBook-Pro.local>
2024-11-05 16:30:54 -0500

    “Updated”

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,7 +11,7 @@ export default function Layout({ children }: LayoutProps) {
   return (
     <html lang="en">
       <head>
-        <meta name='impact-site-verification' value='51c7c560-962e-4d91-ad8e-eec6a297bf53' />
+        <meta name='impact-site-verification' content='51c7c560-962e-4d91-ad8e-eec6a297bf53' />
       </head>
       <body>
         <div className="min-h-screen flex items-center justify-center bg-[#f8f9fa] py-12 px-4 sm:px-6 lg:px-8 font-['Inter',_sans-serif]">
