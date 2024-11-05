bca615514a6713c2e26c8cf2459a226b386a35e3
Adam Aboelmatty <adamaboelmatty@Adams-MacBook-Pro.local>
2024-11-05 15:41:42 -0500

    “Updated”

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,17 +3,18 @@
 import React from "react";
 import "./globals.css";
 
-export default function Layout({ children }) {
-  return (
-    <html>
-        <body>
-            <div
-      className="min-h-screen flex items-center justify-center bg-[#f8f9fa] py-12 px-4 sm:px-6 lg:px-8 font-['Inter',_sans-serif]"
-    >
-      {children}
-    </div>
-        </body>
-    </html>
+interface LayoutProps {
+  children: React.ReactNode;
+}
 
+export default function Layout({ children }: LayoutProps) {
+  return (
+    <html lang="en">
+      <body>
+        <div className="min-h-screen flex items-center justify-center bg-[#f8f9fa] py-12 px-4 sm:px-6 lg:px-8 font-['Inter',_sans-serif]">
+          {children}
+        </div>
+      </body>
+    </html>
   );
 }
